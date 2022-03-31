00e16d8aaad054c99f4bac5e5620356a877b46de
Alberto Chirita <alberto.marian.chirita@gmail.com>
2022-03-31 20:01:34 +0300

    Day 34

---
+++ RESTful_Routes/rest_demo/index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const methodOverride = require("method-override");
+const { v4: uuid } = require("uuid");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.set("views", path.join(__dirname, "/views")); // or just "views" ?
+app.set("view engine", "ejs");
+
+let comments = [
+  {
+    id: uuid(),
+    username: "Todd",
+    comment: "lol that is so funny!",
+  },
+  {
+    id: uuid(),
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    id: uuid(),
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    id: uuid(),
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
+
+// Index
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
+
+// New
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+// Create
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ id: uuid(), username, comment });
+  res.redirect("/comments");
+});
+
+// Show
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
+
+// Edit
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
+// Update
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect("/comments");
+});
+
+// Destroy
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
+});
+
+// ==================================================
+
+app.get("/tacos", (req, res) => {
+  res.send("GET /tacos response");
+});
+
+app.post("/tacos", (req, res) => {
+  const { meat, qty } = req.body;
+  res.send(`Ok, here are your ${qty} ${meat} tacos`);
+});
+
+app.listen(port, () => {
+  console.log(`LISTENING TO PORT ${port}!`);
+});
